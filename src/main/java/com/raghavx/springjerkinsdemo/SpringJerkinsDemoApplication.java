f3f7515f1e7c052d2248ccdec037db911eb2e2cc
Raghvendra Sahu <rx.sahu@gmail.com>
2020-01-12 20:55:05 +0530

    first commit

---
+++ src/main/java/com/raghavx/springjerkinsdemo/SpringJerkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.raghavx.springjerkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJerkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJerkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raghavx/springjerkinsdemo/SpringJerkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghavx.springjerkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJerkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
